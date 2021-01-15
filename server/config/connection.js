5ea39790d991da16b17f6844d23cc8911c067659
gtankha <46304828+gtankha@users.noreply.github.com>
2021-01-14 19:11:36 -0800

    changed dbase name

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/media-store2', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/media-store', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
