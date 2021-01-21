0863a1cf2953fe5a19ac6a8855468d87963806ac
Marco Evangelista <marcobjj@users.noreply.github.com>
2021-01-20 23:12:01 -0800

    test

--- client/src/components/product/index.js
+++ client/src/components/product/index.js
@@ -7,7 +7,7 @@ import { useDispatch } from 'react-redux';
 import moment from 'moment'
 import {UPDATE_BID} from '../../utils/mutations';
 import React, { Component } from 'react';
-
+//test
 function Product(prop) {
     const { _id, image, title, price, description, cart, bidTimeStamp, bidValue, bidderName } = prop;
 
