b51bc71fa80d1aa83b78b3826e9536ba3141dbc4
Ann-Marie <anniedillier@gmail.com>
2021-01-24 16:57:02 -0800

    add catch to fetch

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -37,9 +37,9 @@ workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/i
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
-const APP_PREFIX = 'MediaStore';     
-const VERSION = 'version_01';
-const CACHE_NAME = APP_PREFIX + VERSION
+const APP_PREFIX = "MediaStore";
+const VERSION = "version_01";
+const CACHE_NAME = APP_PREFIX + VERSION;
 const FILES_TO_CACHE = [
   "./index.html",
   "./index.css",
@@ -55,56 +55,62 @@ const FILES_TO_CACHE = [
   "./images/mofia-III.jpg",
   "./images/sekiro.jpg",
   "./images/uncharted.jpg",
-  "./images/watchDogs2.jpg"
+  "./images/watchDogs2.jpg",
 ];
 
 // Respond with cached resources
-self.addEventListener('fetch', function (e) {
-  console.log('fetch request : ' + e.request.url)
+self.addEventListener("fetch", function (e) {
+  console.log("fetch request : " + e.request.url);
   e.respondWith(
     caches.match(e.request).then(function (request) {
-      if (request) { // if cache is available, respond with cache
-        console.log('responding with cache : ' + e.request.url)
-        return request
-      } else {       // if there are no cache, try fetching request
-        console.log('file is not cached, fetching : ' + e.request.url)
-        return fetch(e.request)
-      }
+      if (request) {
+        // if cache is available, respond with cache
+        console.log("responding with cache : " + e.request.url);
+        return request;
+      } else {
+        // if there are no cache, try fetching request
+        console.log("file is not cached, fetching : " + e.request.url);
+        return fetch(e.request).catch(function () {
+          // Do nothing.
+        });
 
-      // You can omit if/else for console.log & put one line below like this too.
-      // return request || fetch(e.request)
+        // You can omit if/else for console.log & put one line below like this too.
+        // return request || fetch(e.request)
+      }
     })
-  )
-})
+  );
+});
 
 // Cache resources
-self.addEventListener('install', function (e) {
+self.addEventListener("install", function (e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
-      console.log('installing cache : ' + CACHE_NAME)
-      return cache.addAll(FILES_TO_CACHE)
+      console.log("installing cache : " + CACHE_NAME);
+      return cache.addAll(FILES_TO_CACHE);
     })
-  )
-})
+  );
+});
 
 // Delete outdated caches
-self.addEventListener('activate', function (e) {
+self.addEventListener("activate", function (e) {
   e.waitUntil(
     caches.keys().then(function (keyList) {
       // `keyList` contains all cache names under your username.github.io
       // filter out ones that has this app prefix to create keeplist
       let cacheKeeplist = keyList.filter(function (key) {
         return key.indexOf(APP_PREFIX);
-      })
+      });
       // add current cache name to keeplist
       cacheKeeplist.push(CACHE_NAME);
 
-      return Promise.all(keyList.map(function (key, i) {
-        if (cacheKeeplist.indexOf(key) === -1) {
-          console.log('deleting cache : ' + keyList[i] );
-          return caches.delete(keyList[i]);
-        }
-      }));
+      return Promise.all(
+        keyList.map(function (key, i) {
+          if (cacheKeeplist.indexOf(key) === -1) {
+            console.log("deleting cache : " + keyList[i]);
+            return caches.delete(keyList[i]);
+          }
+        })
+      );
     })
   );
 });
